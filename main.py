4255311ce9e26452cd6076d53bfb25d77be74956
Vasyl Vaskivskyi <vaskivskyi.v@gmail.com>
2021-04-14 11:51:24 +0100

    handle multiple regions

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ from pathlib import Path
 from subprocess import PIPE, run
 
 sys.path.append(os.getcwd())
-from utils import make_dir_if_not_exists, path_to_str
+from utils import alpha_num_order, make_dir_if_not_exists, path_to_str
 
 
-def main(modality: str, img_dir: Path):
+def run_segmentation(modality: str, img_dir: Path, out_dir: Path):
     cmd_template = "python {wrapper} --img_dir {img_dir} --out_dir {out_dir}"
 
-    out_dir = Path("./output")
     out_dir_deepcell = out_dir / "deepcell"
     out_dir_cellpose = out_dir / "cellpose"
     make_dir_if_not_exists(out_dir_deepcell)
@@ -33,10 +32,48 @@ def main(modality: str, img_dir: Path):
     run(cmd2, shell=True, stdout=PIPE, stderr=PIPE)
 
 
+#
+# def check_if_segm_imgs_in_dir(dir_path: Path):
+#     names = ('nucleus', 'membrane', 'cytoplasm')
+#     listing = list(dir_path.iterdir())
+#     if listing == []:
+#         return listing
+#     else:
+#         found_channels = dict()
+#         for path in listing:
+#             path_str = path_to_str(path)
+#             found = {name: path_str for name in names if name in path_str}
+#             if found != {}:
+#                 found_channels.update(found)
+#         if len(found_channels.keys()) == len(names):
+#             return True
+#         else:
+#             print('Not all channels found in', dir_path,
+#                   'Found channels', found_channels)
+#             return False
+
+
+def collect_img_dirs(dataset_dir: Path):
+    img_dirs = [p for p in list(dataset_dir.iterdir()) if p.is_dir()]
+    img_dirs = sorted(img_dirs, key=lambda path: alpha_num_order(path.name))
+    img_dirs_dict = dict()
+    for img_dir in img_dirs:
+        img_dirs_dict[img_dir.name] = img_dir
+    return img_dirs_dict
+
+
+def main(modality: str, dataset_dir: Path):
+    img_dirs = collect_img_dirs(dataset_dir)
+    out_dir = Path("./output/")
+    for dir_name, dir_path in img_dirs.items():
+        img_out_dir = out_dir / dir_name
+        run_segmentation(modality, dir_path, img_out_dir)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--modality", type=str, help="mibi or celldive")
-    parser.add_argument("--img_dir", type=Path, help="path to directory with images")
+    parser.add_argument("--dataset_dir", type=Path, help="path to directory with images")
     args = parser.parse_args()
 
-    main(args.modality, args.img_dir)
+    main(args.modality, args.dataset_dir)
--- utils.py
+++ utils.py
@@ -1,11 +1,21 @@
 # utils.py>
 import os
+import re
 from os.path import join
 from pathlib import Path
 
 import numpy as np
 import tifffile as tif
 from skimage.segmentation import find_boundaries
+
+
+def alpha_num_order(string: str) -> str:
+    """Returns all numbers on 5 digits to let sort the string with numeric order.
+    Ex: alpha_num_order("a6b12.125")  ==> "a00006b00012.00125"
+    """
+    return "".join(
+        [format(int(x), "05d") if x.isdigit() else x for x in re.split(r"(\d+)", string)]
+    )
 
 
 def path_to_str(path: Path):
@@ -30,8 +40,43 @@ def get_boundary(mask):
     return mask_boundary_indexed
 
 
+def fill_in_ome_meta_template(size_y: int, size_x: int, dtype) -> str:
+    template = """<?xml version="1.0" encoding="utf-8"?>
+            <OME xmlns="http://www.openmicroscopy.org/Schemas/OME/2016-06" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.openmicroscopy.org/Schemas/OME/2016-06 http://www.openmicroscopy.org/Schemas/OME/2016-06/ome.xsd">
+              <Image ID="Image:0" Name="mask.ome.tiff">
+                <Pixels BigEndian="true" DimensionOrder="XYZCT" ID="Pixels:0" SizeC="4" SizeT="1" SizeX="{size_x}" SizeY="{size_y}" SizeZ="1" Type="{dtype}">
+                    <Channel ID="Channel:0:0" Name="cells" SamplesPerPixel="1" />
+                    <Channel ID="Channel:0:1" Name="nuclei" SamplesPerPixel="1" />
+                    <Channel ID="Channel:0:2" Name="cell_boundaries" SamplesPerPixel="1" />
+                    <Channel ID="Channel:0:3" Name="nucleus_boundaries" SamplesPerPixel="1" />
+                    <TiffData FirstC="0" FirstT="0" FirstZ="0" IFD="0" PlaneCount="1" />
+                    <TiffData FirstC="1" FirstT="0" FirstZ="0" IFD="1" PlaneCount="1" />
+                    <TiffData FirstC="2" FirstT="0" FirstZ="0" IFD="2" PlaneCount="1" />
+                    <TiffData FirstC="3" FirstT="0" FirstZ="0" IFD="3" PlaneCount="1" />
+                </Pixels>
+              </Image>
+            </OME>
+        """
+    ome_meta = template.format(size_y=size_y, size_x=size_x, dtype=np.dtype(dtype).name)
+    return ome_meta
+
+
 def save_segmentation_masks(out_dir, cell, nuc, cell_b, nuc_b):
-    tif.imwrite(path_to_str(out_dir / "cell.tif"), cell.astype(np.uint32))
-    tif.imwrite(path_to_str(out_dir / "nucleus.tif"), nuc.astype(np.uint32))
-    tif.imwrite(path_to_str(out_dir / "cell_boundaries.tif"), cell_b.astype(np.uint32))
-    tif.imwrite(path_to_str(out_dir / "nucleus_boundaries.tif"), nuc_b.astype(np.uint32))
+    ome_meta = fill_in_ome_meta_template(cell.shape[0], cell.shape[1], cell.dtype)
+    out_path = path_to_str(out_dir / "mask.ome.tiff")
+    TF = tif.TiffFile(out_path)
+
+    TF.write(
+        cell.astype(np.uint32), contiguous=True, photometric="minisblack", description=ome_meta
+    )
+    TF.write(
+        nuc.astype(np.uint32), contiguous=True, photometric="minisblack", description=ome_meta
+    )
+    TF.write(
+        cell_b.astype(np.uint32), contiguous=True, photometric="minisblack", description=ome_meta
+    )
+    TF.write(
+        nuc_b.astype(np.uint32), contiguous=True, photometric="minisblack", description=ome_meta
+    )
+
+    TF.close()
